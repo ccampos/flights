f8c7862c3f9d28fd9ca46a4db963d8dc2fdcd88a
Claudio Campos <claudio@claudiocampos.com>
2013-02-10 13:51:40 -0500

    add haversine formula to getDistance

--- plan/js/flights.distance.js
+++ plan/js/flights.distance.js
@@ -6,11 +6,33 @@
 * @return {Number} distance
 */
 function getDistance(dep, arr) {
-	var dLat = dep.lat,
-		dLon = dep.lon,
-		aLat = arr.lat,
-		aLon = arr.lon,
+	var	earthRadius = 6371, // in kilometers
+		sqChord,
+		angDist,
+		lat1 = dep.lat,
+		lon1 = dep.lon,
+		lat2 = arr.lat,
+		lon2 = arr.lon,
+		distLat,
+		distLat1,
+		distLat2,
+		distLon,
 		distance;
+
+	// ******* 'haversine' formula *******
+	// to calculate the great-circle distance between two points
+	distLat = (lat2 - lat1).toRad();
+	distLon = (lon2 - lon1).toRad();
+	distLat1 = lat1.toRad();
+	distLat2 = lat2.toRad();
+
+	// square of half the chord length between the points
+	sqChord = Math.sin(distLat / 2) * Math.sin(distLat / 2) + Math.sin(distLon / 2) * Math.sin(distLon / 2) * Math.cos(distLat1) * Math.cos(distLat2);
+
+	// angular distance in radians
+	angDist = 2 * Math.atan2(Math.sqrt(sqChord), Math.sqrt(1 - sqChord));
+
+	distance = Math.round(earthRadius * angDist);
 
 	return distance;
 };
@@ -24,21 +46,38 @@ function getDistance(dep, arr) {
 *	longitude - lon
 */
 function getCoord(location) {
-	var lat,
+	var city,
+		country,
+		lat,
 		lon,
 		coord;
 
-	lat = location.coord.lat.deg + ' ' + location.coord.lat.dir;
-	lon = location.coord.lon.deg + ' ' + location.coord.lon.dir;
+	city = location.city;
+	country = location.country;
+	if (location.coord.lat.dir === 'S') {
+		lat = -location.coord.lat.deg;
+	} else {
+		lat = location.coord.lat.deg;
+	};
+
+	if (location.coord.lon.dir === 'W') {
+		lon = -location.coord.lon.deg;
+	} else {
+		lon = location.coord.lon.deg;
+	};
 
 	coord = {
+		city: city,
+		country: country,
 		lat: lat,
 		lon: lon
 	};
 
 	if ($.isEmptyObject(coord)) {
-		console.error('No coordinates found for', location.city);
+		console.error('No coordinates found for', city);
 	} else {
+		console.warn('remove');
+		console.log(city);
 		return coord;
 	};
 };
