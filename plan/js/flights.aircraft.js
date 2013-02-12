757df16563b0744b31d356eb920758f728f7c894
Claudio Campos <claudio@claudiocampos.com>
2013-02-11 22:44:42 -0500

    created model airbus model object

--- plan/js/flights.aircraft.js
+++ plan/js/flights.aircraft.js
@@ -1,34 +1,44 @@
 // Airbus, Boeing, McDonnell Douglas
 var aircraft = {
 	units: {
-		capacity: liter,
-		ceiling: meter,
-		altitude: meter,
-		range: kilometer,
-		run: meter,
-		speed: kph,
-		time: minute
+		altitude: 'meter',
+		cargo_capacity: 'cubic_meter',
+		fuel_capacity: 'liter',
+		speed: 'kph',
+		range: 'kilometer',
+		service_altitude: 'meter',
+		service_ceiling: 'meter',
+		takeoff_run: 'meter',
+		thrust: 'kilonewton'
 	},
 	jetliners: [
 		{
 			airbus: [
 				{
-					model: A318,
-					cruise: {
-						altitude: 11000,
-						speed: 828
+					model: 'A318',
+					cargo_capacity: 21.21,
+					cockpit_crew: 2,
+					fuel_capacity: 24210,
+					seating: {
+						capacity: {
+							class_1: {
+								max: 132,
+								typical: 117
+							},
+							class_2: 107
+						}
 					},
-					fuel: {
-						capacity: 24210
-					},
-					max: {
-						altitude: 11000,
-						speed: 871
+					speed: {
+						cruise: 828,
+						max: 871
 					},
 					range: 5700,
-					takeoff: {
-						altitude: 0,
-						run: 1828
+					service_ceiling: 12000,
+					service_altitude: 11000,
+					takeoff_run: 1828,
+					thrust: {
+						min: 96,
+						max: 106
 					}
 				}
 			]
